5218e6fe10a0ad0265908324ab55e78ebf5942cd
owentangjintanakarn <owen.t95@hotmail.com>
2020-10-27 17:58:25 -0700

    Search implementation in html get methods implemented

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,12 +5,19 @@
       <b-navbar-brand>Inventory Management System</b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input class="form-control mr-sm-2" placeholder="Search..."></b-form-input>
-          <b-button variant="outline-success my-2 my-sm-2" type="submit">Search</b-button>
+          <b-form-input class="form-control mr-sm-2" placeholder="Search..." v-model="search_term"></b-form-input>
+          <b-button variant="outline-success my-2 my-sm-2" type="submit" v-on:click="getSearch(search_term)">Search</b-button>
         </b-nav-form>
       </b-navbar-nav>
     </b-navbar>
     <h1 class="title text-center">RETAIL INVENTORY MANAGEMENT</h1>
+
+    <div>
+      <b-container>
+        <b-table striped bordered ></b-table>
+      </b-container>
+    </div>
+
   </div>
 </template>
 
@@ -28,19 +35,63 @@ export default {
       products: [],
       product: '',
       search_term: '',
+      error_msg: '',
     }
   },
   mounted() {
-
+    this.getList()
   },
   methods: {
     getList(){
       axios({
         method: 'get',
-
-      })
+        url: '',
+        auth: {
+          username: 'owen_t',
+          password: '2391559Ong'
+        }
+      }).then(response => this.products = response.data)
     },
+    getSearch(term){
+      if (this.search_term !== '' || this.search_term !== null) {
+        axios({
+          method: 'get',
+          url: '' + term,
+          auth: {
+            username: 'owen_t',
+            password: '2391559Ong'
+          }
+        }).then(response => {
+          console.log('INITIAL LOGGED DATA: ', response.data)
+          if (response.data) {
+            this.products = response.data
+          }
+          if (this.products.length === 0) {
+            console.log('EMPTY DATA');
+            this.error_msg = "No results found. Please try again"
+          }
+        })
+          .catch((error) => {
+            if (error.response) {
+              console.log('RESPONSE ERROR');
+              console.log('ERROR DATA'+error.response.data);
+              console.log('ERROR STATUS'+error.response.status);
+              console.log('ERROR HEADERS' + error.response.headers)
+            }else if (error.request) {
+              console.log('REQUEST ERROR');
+              console.log(error.request);
+            }else{
+              console.log('ERROR somewhere', error.message);
+            }
+          })
+      }
+    }
 
-  }
+  },
+  // watch: {
+  //   search_term(value) {
+  //     this.getSearch(value)
+  //   }
+  // }
 }
 </script>
